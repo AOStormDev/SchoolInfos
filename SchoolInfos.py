98fb4ae4258d62dd8adf11a361a2ef13e2d00dd7
Decave <decave27@gmail.com>
2020-12-08 14:19:17 +0900

    add meal

--- SchoolInfos.py
+++ SchoolInfos.py
@@ -1,10 +1,28 @@
 import requests
 import json
-def SchoolInfos(school=str):
-    url = f'https://schoolmenukr.ml/code/api?q={school}'
-    response = requests.get(url)
-    school_infos = json.loads(response.text)
-    return school_infos
+class school:
+    def __init__(self, school : str) -> None:
+        url = f'https://schoolmenukr.ml/code/api?q={school}'
+        response = requests.get(url)
+        school_infos = json.loads(response.text)
+        self.school = school_infos
+    def meal(self):
+        for i in self.school['school_infos']:
+            code = i['code']
+            name = i['name']
+        global school
+        if '초등' in name:
+            school = "elementary"
+        if '중학교' in name:
+            school = "middle"
+        if '고등' in name:
+            school = "high"
+        mealurl = f'https://schoolmenukr.ml/api/{school}/{code}'
+        response = requests.get(mealurl)
+        school_menu = json.loads(response.text)
+        return school_menu
+    
+        
 
 # License 5d-jh
 # https://github.com/5d-jh
